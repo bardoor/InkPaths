97a4a326de27a8ff8c4e37f666dac99b822d4687
samedit66 <samedit66@yandex.ru>
2023-11-14 16:23:53 +0300

    feat(project): Дополнил API для класса Path
    Класс Path теперь самостоятельно можно определять, сформирован ли путь
    внутри него, а также возвращать число элементов в пути и сами элементы.

--- InkPaths/Assets/Scripts/PathBuilder/Path.cs
+++ InkPaths/Assets/Scripts/PathBuilder/Path.cs
@@ -3,20 +3,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Чернильный путь.
+/// </summary>
 public class Path
 {
-    private List<PathElement> pathElements = new List<PathElement>();
-    public List<PathElement> GetPath()
-    {
-        return pathElements;
-    }
-    public void AddElement(PathElement element)
-    {
-        pathElements.Add(element);
-    }
+    /// <summary>
+    /// Список элементов, из которых состоит данный путь.
+    /// </summary>
+    private List<PathElement> _pathElements;
 
-    public static object Combine(string dataPath, string v)
+    /// <summary>
+    /// Элементы в пути.
+    /// </summary>
+    public List<PathElement> PathElements { get => return new List<PathElement>(_pathElements); }
+
+    /// <summary>
+    /// Число элементов в пути.
+    /// </summary>
+    public int Count { get => return _pathElements.Count(); }
+
+    /// <summary>
+    /// Конструктор, создающий пустой путь.
+    /// </summary>
+    public Path() => _pathElements = new List<PathElement>();
+    
+    /// <summary>
+    /// Конструктор, создающий путь на основе списка элементов пути.
+    /// </summary>
+    /// <param name="elements">Список элементов пути.</param>
+    public Path(List<PathElement> elements) => _pathElements = new List<PathElement>(elements);
+
+    /// <summary>
+    /// Добавляет новый элемент в путь.
+    /// </summary>
+    /// <param name="element">Новый элемент.</param>
+    public void AddElement(PathElement element) => _pathElements.Add(element);
+
+    /// <summary>
+    /// Удаляет все элементы из пути.
+    /// </summary>
+    public void Clear() => _pathElements.Clear();
+
+    /// <summary>
+    /// Копирует путь.
+    /// </summary>
+    /// <returns>Копия пути.</returns>
+    public Path Copy() => return new Path(_pathElements);
+
+    /// <summary>
+    /// Проверяет, что путь является завершенным: начало и конец пути это чернильные точки.
+    /// </summary>
+    /// <returns>Признак, завершен ли путь.</returns>
+    public bool IsFinishedPath()
     {
-        throw new NotImplementedException();
+        if (this.Count < 3)
+        {
+            return false;
+        }
+        
+        return (_pathElements.First() is InkBlob) && (_pathElements.Last() is IntBlob);
     }
 }
