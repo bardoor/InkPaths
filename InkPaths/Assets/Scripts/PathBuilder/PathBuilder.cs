66ac0c0d21a4f4aca8c827ee21a2639f5dc57581
samedit66 <samedit66@yandex.ru>
2023-11-15 01:42:16 +0300

    feat(project): Добавил API клаccy PathBuilder
    PathBuilder теперь может хранить все созданные пути, а также оповещать слушателей о создании/разрушении путей

--- InkPaths/Assets/Scripts/PathBuilder/PathBuilder.cs
+++ InkPaths/Assets/Scripts/PathBuilder/PathBuilder.cs
@@ -2,41 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathBuilder
+public class PathBuilder : IObservable
 {
-    private static PathBuilder instance;
+    private static PathBuilder _instance;
 
-    private InkPath[] _paths { get; }
+    private HashSet<InkPath> _paths { get; }
 
     private InkPath _currentPath;
 
-    private PathBuilder() { }
+    private HashSet<IObserver> _observers;
+
+    private PathBuilder() {
+        _paths = new HashSet<InkPath>();
+        _currentPath = new InkPath();
+        _observers = new HashSet<IObserver>();
+    }
 
     public static PathBuilder Instance
     {
         get
         {
-            if (instance == null)
+            if (_instance == null)
             {
-                instance = new PathBuilder();
+                _instance = new PathBuilder();
             }
-            return instance;
+            return _instance;
         }
     }
     
     public void CancelBuilding()
     {
+        // ќчистить активный путь
+        _currentPath.Clear();
 
-    }
-
-    public void StartBuild(PathElement element) 
-    {
-
+        // ”ведомить слушателей, что путь разрушилс€
+        NotifyObservers(new CancelledBuildingPath());
     }
 
     public void AddElement(PathElement element)
     {
+        // ƒобавить новый элемент в конец активного пути
+        _currentPath.AddElement(element);
 
+        // ќбновить состо€ние элементов, окружающих последний добавленный элемент
+        element.UpdateElementsStatesAround();
+
+        // ≈сли активный путь €вл€етс€ законченным путем...
+        if (_currentPath.IsFinishedPath())
+        {
+            // ...поместить активный путь в список созданных путей
+            _paths.Add(_currentPath.Copy());
+            // ...очистить активный путь
+            _currentPath.Clear();
+            // ...уведомить слушателей, что создалс€ новый путь
+            NotifyObservers(new FinishedBuildingPath());
+        }
     }
 
+    public void AddObserver(IObserver o) => _observers.Add(o);
+
+    public void RemoveObserver(IObserver o) => _observers.Remove(o);
+
+    public void NotifyObservers(IEvent e)
+    {
+        foreach (IObserver o in _observers)
+        {
+            observer.Update(e);
+        }
+    }
 }
