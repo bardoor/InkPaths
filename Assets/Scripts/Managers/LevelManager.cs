8fc2e41bbd6ad1825dc905aa6440715c4a625562
bardoor <93031169+bardoor@users.noreply.github.com>
2023-11-28 01:54:38 +0300

    Добавил менеджер уровней

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class LevelManager : ScriptableObject
+{
+    [SerializeField]
+    private GameObject _currentLevel;
+    
+    public void StartLevel(int number)
+    {
+        EndLevel();
+        _currentLevel = ResourceManager.LoadLevel(number);
+        if (_currentLevel == null)
+        {
+            Debug.Log($"Can't load Level_{number}! It was not found!!!");
+        }
+        Instantiate(_currentLevel, Vector3.zero, Quaternion.identity);
+    }
+
+    private void EndLevel()
+    {
+        Destroy(_currentLevel);
+        _currentLevel = null;
+    }
+
+}
