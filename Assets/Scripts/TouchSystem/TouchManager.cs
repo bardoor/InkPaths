eddc4822259bc5c61e69fb99790cb0af79ddacc9
samedit66 <samedit66@yandex.ru>
2023-11-28 21:08:23 +0300

    feat(project): зачаток TouchManager

--- Assets/Scripts/TouchSystem/TouchManager.cs
+++ Assets/Scripts/TouchSystem/TouchManager.cs
@@ -2,17 +2,95 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using System;
+using UnityEngine.InputSystem.Interactions;
+using UnityEngine.AdaptivePerformance;
 
 public class TouchManager : MonoBehaviour
 {
+    private TouchAction _action;
+
+    private void Awake()
+    {
+        _action = new TouchAction();
+    }
+
+    private void OnEnable()
+    {
+        _action.Enable();
+        _action.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        _action.Touch.TouchPress.canceled += ctx => EndTouch(ctx);
+        _action.Touch.TouchHold.performed += ctx => HoldTouch(ctx);
+    }
+
+    private void OnDisable()
+    {
+        _action.Touch.TouchPress.started -= ctx => StartTouch(ctx);
+        _action.Touch.TouchPress.canceled -= ctx => EndTouch(ctx);
+        _action.Touch.TouchHold.performed -= ctx => HoldTouch(ctx);
+        _action.Disable();
+    }
+
+    private void StartTouch(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Touch started " + _action.Touch.TouchPosition.ReadValue<Vector2>());
+    }
+
+    private void EndTouch(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Touch ended");
+    }
+
+    private void HoldTouch(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("HOLDING");
+    }
+
+    /*
     private PlayerInput _playerInput;
+
+    private InputAction _touchPressAction;
 
     private InputAction _touchPositionAction;
 
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
+        _touchPressAction = _playerInput.actions.FindAction("TouchPress");
         _touchPositionAction = _playerInput.actions.FindAction("TouchPosition");
+    }
+
+    private void OnEnable()
+    {
+        _touchPositionAction.performed += TouchPressed;
+        _touchPositionAction.canceled += TouchUnpressed;
+    }
+
+    private void OnDisable()
+    {
+        _touchPositionAction.performed -= TouchPressed;
+        _touchPositionAction.canceled -= TouchUnpressed;
+    }
+
+    private void TouchUnpressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Finished pressing");
+    }
+
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("Called TouchPressed");
+
+        Ray ray = Camera.main.ScreenPointToRay(context.ReadValue<Vector2>());
+        RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray);
+
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider == null) continue;
+
+            hit.collider.TryGetComponent(out PathElement pathElement);
+            //pathElement.onPressed();
+        }
     }
 
     private Vector3 getPositionOnScreen(InputAction action)
@@ -25,22 +103,7 @@ public class TouchManager : MonoBehaviour
 
     private void Update()
     {
-        if (_touchPositionAction.WasPressedThisFrame())
-        {
-            Vector3 position = getPositionOnScreen(_touchPositionAction);
-            Debug.Log("Pressed " + position);
-        }
-
-        else if (_touchPositionAction.WasPerformedThisFrame())
-        {
-            Vector3 position = getPositionOnScreen(_touchPositionAction);
-            Debug.Log("Holding " + position);
-        }
-
-        else if (_touchPositionAction.WasReleasedThisFrame())
-        {
-            Vector3 position = getPositionOnScreen(_touchPositionAction);
-            Debug.Log("Released " + position);
-        }
+        
     }
+    */
 }
