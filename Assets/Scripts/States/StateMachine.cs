09d638ad41f674729ab31380126ee6fbcb552d75
bardoor <93031169+bardoor@users.noreply.github.com>
2023-12-10 04:11:12 +0300

    feat(project): добавил в машину состояний контроль переходов
    Переходы из состояний теперь задаются словарем

--- Assets/Scripts/States/StateMachine.cs
+++ Assets/Scripts/States/StateMachine.cs
@@ -1,13 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class StateMachine
 {
     public PathElementState CurrentState { get; set; }
     private PathElement _element;
+    private Dictionary<Type, Type[]> _validStateTransitions;
 
-    public void Initialize(PathElement element, PathElementState startState)
+    public void SetValidStateTransitions(Type[] stateTransitionCycle)
+    {
+        _validStateTransitions = stateTransitionCycle
+            .Zip(stateTransitionCycle.Skip(1), (curState, nextState) => new {key = curState, value = nextState})
+            .ToDictionary(pair => pair.key, pair => new Type[] { pair.value });
+    }
+
+    public bool TransitionIsValid(State newState)
+    {
+        return IsValidTransition(CurrentState, newState);
+    }
+
+    private bool IsValidTransition(State startState, State nextState)
+    {
+        Type[] validTransitions = GetValidTransitions(startState);
+        return validTransitions.Contains(nextState.GetType());
+    }
+
+    private Type[] GetValidTransitions(State startState)
+    {
+        Type startStateType = startState.GetType();
+        Type[] validTransitions = new Type[] { };
+
+        if (_validStateTransitions.ContainsKey(startStateType))
+        {
+            validTransitions = _validStateTransitions[startStateType];
+        }
+
+        return validTransitions;
+    }
+
+    public virtual void Initialize(PathElement element, PathElementState startState)
     {
         _element = element;
         startState.Element = element;
@@ -15,15 +49,17 @@ public class StateMachine
         CurrentState.Enter();
     }
 
-    public void ChangeState(PathElementState newState)
+    public virtual bool ChangeState(PathElementState newState)
     {
-        if (newState.GetType() == CurrentState.GetType())
-            return;
+        if (!IsValidTransition(CurrentState, newState))
+            return false;
 
         CurrentState.Exit();
         newState.Element = _element;
         CurrentState = newState;
         CurrentState.Enter();
         _element.ReportStateChanged();
+
+        return true;
     }
 }
